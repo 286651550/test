285a04ddb0eb51415d7f8037e2429f5fdf7e3326
28665150 <286651550@qq.com>
2018-05-10 22:16:29 +0800

    a8

--- zqxt_admin/settings.py
+++ zqxt_admin/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
 STATICFILES_DIRS= (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'common_static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 )
--- zqxt_admin/urls.py
+++ zqxt_admin/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from blog import views
 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/', views.index),
