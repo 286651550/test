9fbd0ed91b6d54ced0d57d319135d0739848a1cd
28665150 <286651550@qq.com>
2018-05-10 22:36:55 +0800

    a9

--- zqxt_admin/settings.py
+++ zqxt_admin/settings.py
@@ -122,13 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS= (
-    os.path.join(BASE_DIR, 'common_static'),
-)
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
-)
+# STATICFILES_DIRS= (
+#     os.path.join(BASE_DIR, 'common_static'),
+# )
